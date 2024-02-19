c15d0984992228dc018130b92b2778374a4ad609
Todd Fredrich <tfredrich@gmail.com>
2024-02-19 10:34:37 -0700

    Introduced PathIndex for route search optimization (later).

---
+++ core/src/main/java/org/restexpress/route/index/PathIndex.java
@@ -0,0 +1,158 @@
+package org.restexpress.route.index;
+
+import static org.restexpress.route.index.SlashSegment.SLASH;
+
+import java.util.Arrays;
+
+public class PathIndex<T>
+{
+	private PathSegment<T> root;
+
+	public PathIndex()
+	{
+		super();
+		this.root = new SlashSegment<>();
+	}
+
+	public void insert(String path, T object)
+	{
+		String[] pathSegments = asPathSegments(path);
+		PathSegment<T> currentNode = root;
+
+		for (String segment : pathSegments)
+		{
+			// This accounts for a leading slash ('/') on the path that creates an empty segment in the array.
+			if (segment.isBlank()) continue;
+
+			currentNode = getOrCreateSlashNode(currentNode);
+
+			if (isIdentifier(segment))
+			{
+				currentNode = insertIdentifierSegment(currentNode, segment);
+			}
+			else
+			{
+				currentNode = insertPathSegment(currentNode, segment);
+			}
+		}
+
+		currentNode.setIsLeaf(true);
+		currentNode.setIndexedObject(object);
+	}
+
+	public SearchResults<T> search(String path)
+	{
+		String[] pathSegments = asPathSegments(path);
+		PathSegment<T> currentNode = root;
+		SearchResults<T> results = new SearchResults<>(path);
+
+		for (String segment : Arrays.asList(pathSegments))
+		{
+			// This accounts for a leading slash ('/') on the path that creates an empty segment in the array.
+			if (segment.isBlank()) continue;
+
+			currentNode = traverse(currentNode, segment);
+
+			if (currentNode != null)
+			{
+				if (currentNode.isIdentifierNode())
+				{
+					results.addIdentifier(((IdentifierSegment<T>) currentNode).getParameterName(), segment);
+				}
+				else
+				{
+					results.addSegment(segment);
+				}
+			}
+			else
+			{
+				results.setFailed();
+				return results;
+			}
+		}
+
+		if (currentNode == null || !currentNode.isLeaf())
+		{
+			results.setFailed();
+		}
+		else if (currentNode.hasIndexedObject())
+		{
+			results.setObject(currentNode.getIndexedObject());
+		}
+
+		return results;
+	}
+
+	private PathSegment<T> traverse(PathSegment<T> node, String pathSegment)
+	{
+		if (node == null || pathSegment == null) return null;
+
+		// If the current node is a slash node, move to its child node
+		if (!node.isSlashNode())
+	    {
+	    	node = node.getChild(SLASH);
+	    	if (node == null) return null;
+	    }
+
+		 // Find the child node that matches the segment
+	    PathSegment<T> childNode = node.getChild(pathSegment);
+
+
+	    // If there is not a segment match attempt to find an identifier segment
+	    return childNode != null ? childNode : node.getChild(IdentifierSegment.WILDCARD);
+	}
+
+	private String[] asPathSegments(String path)
+	{
+		return path.split(SLASH);
+	}
+
+	private boolean isIdentifier(String segment)
+	{
+		return IdentifierSegment.isIdentifier(segment);
+	}
+
+	private PathSegment<T> insertIdentifierSegment(PathSegment<T> currentNode, String pathSegment)
+	{
+		IdentifierSegment<T> childNode = (IdentifierSegment<T>) currentNode.getChild(IdentifierSegment.WILDCARD);
+
+		if (childNode == null)
+		{
+			childNode = new IdentifierSegment<>(pathSegment);
+			currentNode.addChild(childNode);
+		}
+
+		return childNode;
+	}
+
+	private PathSegment<T> insertPathSegment(PathSegment<T> currentNode, String pathSegment)
+	{
+		PathSegment<T> childNode = currentNode.getChild(pathSegment);
+
+		if (childNode == null)
+		{
+			childNode = currentNode.addChild(new PathSegment<>(pathSegment));
+		}
+
+		return childNode;
+	}
+
+	private PathSegment<T> getOrCreateSlashNode(PathSegment<T> currentNode)
+	{
+		if (!currentNode.isSlashNode())
+		{
+			PathSegment<T> slashNode = currentNode.getChild(SLASH);
+
+			if (slashNode != null)
+			{
+				return slashNode;
+			}
+			else
+			{
+				return currentNode.addChild(new SlashSegment<>());
+			}
+		}
+
+		return currentNode;
+	}
+}
---
+++ core/src/main/java/org/restexpress/route/index/SearchResults.java
@@ -0,0 +1,131 @@
+package org.restexpress.route.index;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class SearchResults<T>
+{
+	private String path;
+	private List<String> segments;
+	private boolean success = true;
+	private Map<String, String> identifiers;
+	private T object;
+
+	public SearchResults(String path)
+	{
+		super();
+		this.path = path;
+		setFailed();
+	}
+
+	public SearchResults(String path, List<String> segments, Map<String, String> identifiers, T object, boolean success)
+	{
+		super();
+		this.path = path;
+
+		if (segments != null) segments.stream().forEach(this::addSegment);
+		if (identifiers != null) identifiers.entrySet().stream().forEach(e -> addIdentifier(e.getKey(), e.getValue()));
+		setObject(object);
+		this.success = success;
+	}
+
+	protected void setFailed()
+	{
+		this.success = false;
+	}
+
+	public boolean matched()
+	{
+		return success;
+	}
+
+	protected void addSegment(String segment)
+	{
+		if (segments == null)
+		{
+			segments = new ArrayList<>();
+		}
+
+		segments.add(segment);
+	}
+
+	protected void addIdentifier(String parameterName, String segment)
+	{
+		addSegment(parameterName);
+
+		if (identifiers == null)
+		{
+			identifiers = new HashMap<>();
+		}
+
+		identifiers.put(parameterName, segment);
+	}
+
+	public String getIdentifier(String parameterName)
+	{
+		return (hasIdentifiers() ? identifiers.get(parameterName) : null);
+	}
+
+	public boolean hasIdentifiers()
+	{
+		return (identifiers != null);
+	}
+
+	public Map<String, String> getIdentifiers()
+	{
+		return identifiers;
+	}
+
+	public T getObject()
+	{
+		return object;
+	}
+
+	public boolean hasIndexedObject()
+	{
+		return (object != null);
+	}
+
+	public String getPath()
+	{
+		return path;
+	}
+
+	protected void setObject(T object)
+	{
+		this.object = object;
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder(path);
+		sb.append(": ");
+
+		if (matched())
+		{
+			StringJoiner joiner = new StringJoiner("/", "/", "");
+			segments.forEach(joiner::add);
+			sb.append(joiner.toString());
+
+			if (hasIdentifiers())
+			{
+				sb.append(" : ");
+				sb.append(identifiers.toString());
+			}
+
+			if (hasIndexedObject())
+			{
+				sb.append(" (");
+				sb.append(getObject().toString());
+				sb.append(")");
+			}
+			return sb.toString();
+		}
+
+		sb.append("*** FAILED ***");
+		return sb.toString();
+	}
+}
